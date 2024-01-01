65b99552368328a743c9c2f3fec42f54a366e179
unknown <dubeypratham11@gmail.com>
2024-01-01 21:24:08 +0530

    sidenav

--- client/src/pages/Userdashboard/Profile.jsx
+++ client/src/pages/Userdashboard/Profile.jsx
@@ -2,7 +2,7 @@
 const Profile = () => {
   return (
     <div className="flex justify-center items-center">
-      <div className="w-[450px] "></div>
+      <div className="w-[25%] "></div>
       <div className=" text-8xl">sssssssssssssssssssssssssssssssssss</div>
     </div>
   )
